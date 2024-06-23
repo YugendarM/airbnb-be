f1c7fd75eb7e3a99787cbb8297b2f141a2a270c1
YugendarM <yugendar2411@gmail.com>
2024-06-23 16:22:09 +0530

    Add - added feature to add properties to wishlist

--- middlewares/authenticateUser.js
+++ middlewares/authenticateUser.js
@@ -1,15 +1,18 @@
 const jwt = require("jsonwebtoken")
 
 const authenticateUser = (request, response, next) => {
-    console.log(request.headers.authorization)
+    console.log("from header"+request.headers.authorization)
     const authHeader = request.headers.authorization
+    console.log("authheader", authHeader)
     const authToken = authHeader && authHeader.split(" ")[1]
-    console.log(authToken)
+    console.log("auth"+authToken)
     if(!authToken){
+        console.log("token not found")
         return response.status(404).send({message: "Token not found"})
     }
     jwt.verify(authToken, process.env.JWT_TOKEN, (error, data) => {
         if(error){
+            console.log("error in verify")
             return response.status(404).send({message: "User not authorized"})
         }
         console.log(data)
